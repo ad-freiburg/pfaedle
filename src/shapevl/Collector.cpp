bda37e8f79eb915568ab9c5277f93fcae56f1913
Patrick Brosi <info@patrickbrosi.de>
2025-05-12 09:05:00 +0200

    output route type

--- src/shapevl/Collector.cpp
+++ src/shapevl/Collector.cpp
@@ -230,14 +230,14 @@ double Collector::add(const Trip* oldT, const Shape* oldS, size_t numOldTrips,
     } else if (_reportLevel == 2) {
       (*_reportOut) << std::fixed << std::setprecision(6);
       (*_reportOut) << oldT->getId() << "\t" << AN << "\t" << AL << "\t"
-                    << avgFd << "\t" << fd << "\t"
-                    << d
-                    << "\t"
-                    << lenDiff
+                    << avgFd << "\t" << fd << "\t" << d << "\t" << lenDiff
                     << "\t" << util::geo::getWKT(oldSegs) << "\t"
                     << util::geo::getWKT(newSegs) << "\t"
-                    << oldT->getRoute()->getShortName() << "\t"
-                    << numOldTrips << "\t";
+                    << oldT->getRoute()->getShortName() << "\t" << numOldTrips
+                    << "\t"
+                    << ad::cppgtfs::gtfs::flat::Route::getTypeString(
+                           oldT->getRoute()->getType())
+                    << "\t";
 
       for (const auto& st : oldT->getStopTimes()) {
         (*_reportOut) << st.getStop()->getName() << "\t"
@@ -512,7 +512,8 @@ std::map<string, double> Collector::getStats() {
 
 // _____________________________________________________________________________
 std::pair<size_t, double> Collector::getDa(const std::vector<LINE>& a,
-                                           const std::vector<LINE>& b, double segLen) {
+                                           const std::vector<LINE>& b,
+                                           double segLen) {
   assert(a.size() == b.size());
   std::pair<size_t, double> ret{0, 0};
 
